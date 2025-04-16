f2b58f54a46441b7f7ecedcd058a0c6ceb52ba49
durga-9877 <11229A003@kanchiuniv.ac.in>
2025-04-16 21:31:13 +0530

    Add files via upload

---
+++ Edge Detection.py
@@ -0,0 +1,13 @@
+import cv2 
+import numpy as np 
+image = cv2.imread(r"C:\Users\A.Durga sankar sai\OneDrive\Desktop\images.jpg") 
+if image is None:
+    print("Error: Could not open or find the image.") 
+    exit() 
+blurred_image = cv2.GaussianBlur(image, (5, 5), 0)
+edges = cv2.Canny(blurred_image, 50, 150) 
+
+cv2.imshow('Original Image', image) 
+cv2.imshow('Edge Detection', edges) 
+cv2.waitKey(0) 
+cv2.destroyAllWindows()
